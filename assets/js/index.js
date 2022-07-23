b59ec15abf45059ab1a020bdbae0a332f056856b
duyduc2003 <dangduyducdangduyduc@gmail.com>
2022-07-23 23:50:46 +0700

    sub nav #1

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,30 +4,117 @@ const $ = document.querySelector.bind(document);
 const btnToggleIntervalSlider = $('.btn-toggle-interval');
 const sliderImg = $('#slider-img');
 
+
 const app = {
 
+    subNavHeader() {
+
+        tippy('#my-tippy', {
+            hideOnClick: 'toggle',
+            trigger: "click",
+            interactive: true,
+            // offset: [0, 10],
+            placement: 'bottom',
+            appendTo: () => $('#my-tippy'),
+            popperOptions: {
+                strategy: 'fixed',
+            },
+            zIndex: 9999,
+            render(instance) {
+                const popper = document.createElement('div');
+                popper.classList.add('popper');
+                // popper.style.top = '100%'
+                const box = document.createElement('div');
+                popper.appendChild(box);
+                box.className = 'box-tippy';
+                box.innerHTML = /*html*/`
+                <div class="wrapper">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col col-2">
+                                <div class="footer-heading sub-heading" >Phần mềm</div>
+                                <ul class="footer-list list-unstyled">
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Ứng dụng cho Windows</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Outlook</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Microsoft Teams</a></li>
+                                </ul>
+                            </div>
+                            <div class="col col-2">
+                                <div class="footer-heading sub-heading" >Máy tính và các thiết bị</div>
+                                <ul class="footer-list list-unstyled">
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Mua Xbox</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Phụ kiện Máy tính</a></li>
+                                </ul>
+                            </div>
+                            <div class="col col-2">
+                                <div class="footer-heading sub-heading" >Giải trí</div>
+                                <ul class="footer-list list-unstyled">
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Trò chơi trên PC</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Trò chơi trên Windows</a></li>
+                                </ul>
+                            </div>
+                            <div class="col col-2">
+                                <div class="footer-heading sub-heading" >Kinh doanh</div>
+                                <ul class="footer-list list-unstyled">
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Đám mây Microsoft</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Microsoft Security</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Azure</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Dynamics 365</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Microsoft 365 dành cho doanh nghiệp</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Microsoft trong ngành</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Microsoft Power Platform</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Windows 365</a></li>
+                                </ul>                                               
+                            </div>
+                            <div class="col col-2">
+                                <div class="footer-heading sub-heading" >Nhà phát triển & CNTT</div>
+                                <ul class="footer-list list-unstyled">
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Trung tâm nhà phát triển</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Hướng dẫn sử dụng</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Microsoft Learn</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Cộng đồng Microsoft Tech</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Azure Marketplace</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">AppSource</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Visual Studio</a></li>
+                                </ul>   
+                            </div>                                                               
+                            <div class="col col-2">
+                                <div class="footer-heading sub-heading" >Khác</div>
+                                <ul class="footer-list list-unstyled">
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Bản tải xuống và bảo mật miễn phí</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Giáo dục</a></li>
+                                    <li class="footer-item sub-item"><a href="#" class="footer-link">Licensing</a></li>
+                                </ul>  
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+
+
+                function onUpdate(prevProps, nextProps) {
+                    // DOM diffing
+                    if (prevProps.content !== nextProps.content) {
+                        box.textContent = nextProps.content;
+                    }
+                }
+                return {
+                    popper,
+                    onUpdate
+                }
+            },
+            onClickOutside(instance, event) {
+                instance.hide()
+            },
+        })
+    },
     handleClickBtnToggleIntervalSlider() {
-        let isPlay = true;
-        const classNameActive = 'btn-toggle-interval--active';
-        btnToggleIntervalSlider.onclick = e => {
-            if (isPlay) {
-                btnToggleIntervalSlider.classList.add(classNameActive);
-                new bootstrap.Carousel(sliderImg, {
-                    interval: false,
-                })
-                isPlay = false;
-            } else {
-                btnToggleIntervalSlider.classList.remove(classNameActive);
-                new bootstrap.Carousel(sliderImg, {
-                    interval: 2000,
-                })
-                isPlay = true;
-            }
-        }
+
+
     },
 
     init() {
-        this.handleClickBtnToggleIntervalSlider()
+        this.handleClickBtnToggleIntervalSlider();
+        this.subNavHeader()
     },
 };
 
